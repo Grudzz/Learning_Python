22606a7091d6bed2a3f68b20d0a6fa1d55c8a501
Grudzz <65249161+Grudzz@users.noreply.github.com>
2020-05-12 15:03:45 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("what is your name")
+myName = input() # Taking user input 
+print("Your name, " + myName)
+print(len(myName)) # Getting the length of myName string
+
+print("What is your age?")
+myAge = input()
+print("In one year, you will be " + str(int(myAge) + 1) + " congrats!") #convert input str to int
