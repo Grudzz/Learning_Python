2be888451726e9f08e982347a41339afb546ae6f
Grudzz <65249161+Grudzz@users.noreply.github.com>
2020-05-15 14:55:20 -0400

    Add files via upload

---
+++ Class2.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""
+
+def myfunc1(): # Could name anything
+    print("Hello")
+ 
+print("yo yo yo")
+myfunc1()
+
+
+def you(x):
+    print("you func")
+    return 7 * x
+
+you_var = you(4)
+print(you_var)
+
+
+def logibear(x, y):
+    return x + y
+
+
+batch1 = logibear(3, 7)
+print(batch1)
+print(logibear(3, 7))
+
+
+def new:
+    
+    
+ 
+
+
+def special_func(z):
+    print(z)
+    
+special_func(69)
+special_func("Can we get a string")
+special_func(69.72) 
+
+
+print("How old are you?")
+age = int(input())
+
+def age_func(age):
+    if age >= 18:
+        print("You're legal for stuff")
+    else:
+        print("No 18+ stuff for you")
+        
+age_func(age)
+        
+   """      
+        
